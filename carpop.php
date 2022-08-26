b53e10465916376743f8542042a54037e3705298
r0adra93 <github.com@legendsclan.org>
2022-08-26 09:52:57 -0400

    removed recurring function
    removed recurring function from all scripts

--- carpop.php
+++ carpop.php
@@ -28,35 +28,4 @@ if(fwrite($fh,$input)){
 }else{
  echo "<html><head><title>vehpop Sad Potato</title></head><body><h1>Sad Potato</h1><p>The vehicles.lua file was <em>not</em> updated.</p></body></html>";
 }
-function sqlrun($query){
-  global $dbinfo;
-  global $messages;
-  global $lang;
-  $dbcon = NULL;
-  $result = array();
-  $row = array();
-  $return = array();
-  $dbcon = new mysqli($dbinfo['host'],$dbinfo['username'],$dbinfo['password'],$dbinfo['dbname']);
-  if($dbcon->connect_error){
-   if(R0ADBUG !== NULL){$messages[] = $dbcon->connect_error;}
-   $messages[] = $lang['ersql'];
-  }
-  if($result = $dbcon->query($query)){
-   if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-     $return[] = $row;
-    }
-   }else{$return[0]['empty'] = "empty";}
-   $result->free();
-  }else{
-   $return['error'] = "true";
-   if(R0ADBUG !== NULL){
-    $messages[] = $dbcon->error;
-    $messages[] = $query;
-   }
-   $messages[] = $lang['ersql'];
-  }
-  $dbcon->close();
-  return($return);
- }
 ?>
