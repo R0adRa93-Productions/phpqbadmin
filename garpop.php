cd09e56c2594a6e3a753a633504aaadbce1379aa
r0adra93 <github.com@legendsclan.org>
2022-08-26 10:13:04 -0400

    Update garpop.php
    removed test output writing to garages.conf

--- garpop.php
+++ garpop.php
@@ -1,33 +1,29 @@
 <?php
-require("includes/config.inc.php");
-require("includes/functions.inc.php");
-$query = null;
-$input = null;
-$timestamp = null;
-$garconf = array();
-$garloc = array();
-$query = "SELECT * FROM `qb-garages-config`";
-$garconf = sqlrun($query);
-$query = "SELECT * FROM `qb-garages-locations`";
-$garloc = sqlrun($query);
-//$fh = fopen("C:\\Program Files\\FXServer\\txData\\QBCoreFramework\\resources\\[qb]\\qb-garages\\config.lua",'w');
-$timestamp = mktime(date('H'),date('i'),date('s'),date('m'),date('d'),date('Y'));
-$timestamp = date('YmdHis', $timestamp);
-$input = "-- Date and Time Edited: " . date("Y-m-d H:i:s") . "\r";
-$input .= "AutoRespawn = {$garconf[0]['autorespawn']}
-SharedGarages = {$garconf[0]['sharedgarages']}
-VisuallyDamageCars = {$garconf[0]['visuallydamagecars']}\r";
-foreach($garloc as $k => $v){
-  $input .= "<br /> ['{$v['qb-garages-location_id']}'] = {['label'] = '{$v['label']}',['putVehicle'] = '{$v['putvehicle']}',['takeVehicle'] = '{$v['takevehicle']}',['spawnVehicle'] = '{$v['spawnvehicle']}',['showBlip'] = '{$v['showblip']}',['blipNumber'] = '{$v['blipnumber']},['type'] = '{$v['type']}',['vehicle'] = '{$v['vehicle']}',['job'] = '{$v['job']}'},\r";
-}
-echo $input;
-echo "<h1></h1>";
-/*
-if(fwrite($fh,$input)){
- echo "<html><head><title>vehpop Happy Potato</title></head><body><h1>Happy Potato</h1><p>The vehicles.lua file was <em>successfully</em> updated.</p></body></html>";
- fclose($fh);
-}else{
- echo "<html><head><title>vehpop Sad Potato</title></head><body><h1>Sad Potato</h1><p>The vehicles.lua file was <em>not</em> updated.</p></body></html>";
-}
-*/
+ require("includes/config.inc.php");
+ require("includes/functions.inc.php");
+ $query = null;
+ $input = null;
+ $timestamp = null;
+ $garconf = array();
+ $garloc = array();
+ $query = "SELECT * FROM `qb-garages-config`";
+ $garconf = sqlrun($query);
+ $query = "SELECT * FROM `qb-garages-locations`";
+ $garloc = sqlrun($query);
+ //$fh = fopen("C:\\Program Files\\FXServer\\txData\\QBCoreFramework\\resources\\[qb]\\qb-garages\\config.lua",'w');
+ $timestamp = mktime(date('H'),date('i'),date('s'),date('m'),date('d'),date('Y'));
+ $timestamp = date('YmdHis', $timestamp);
+ $input = "-- Date and Time Edited: " . date("Y-m-d H:i:s") . "\r";
+ $input .= "AutoRespawn = {$garconf[0]['autorespawn']}
+ SharedGarages = {$garconf[0]['sharedgarages']}
+ VisuallyDamageCars = {$garconf[0]['visuallydamagecars']}\r";
+ foreach($garloc as $k => $v){
+  $input .= "['{$v['qb-garages-location_id']}'] = {['label'] = '{$v['label']}',['putVehicle'] = '{$v['putvehicle']}',['takeVehicle'] = '{$v['takevehicle']}',['spawnVehicle'] = '{$v['spawnvehicle']}',['showBlip'] = '{$v['showblip']}',['blipNumber'] = '{$v['blipnumber']},['type'] = '{$v['type']}',['vehicle'] = '{$v['vehicle']}',['job'] = '{$v['job']}'},\r";
+ }
+ if(fwrite($fh,$input)){
+  echo "<html><head><title>vehpop Happy Potato</title></head><body><h1>Happy Potato</h1><p>The vehicles.lua file was <em>successfully</em> updated.</p></body></html>";
+  fclose($fh);
+ }else{
+  echo "<html><head><title>vehpop Sad Potato</title></head><body><h1>Sad Potato</h1><p>The vehicles.lua file was <em>not</em> updated.</p></body></html>";
+ }
 ?>
