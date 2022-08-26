3dbcfa3a62a4a2efa29f4022bc00d3e1b5497d72
r0adra93 <github.com@legendsclan.org>
2022-08-26 13:56:36 -0400

    renamed files
    Renamed files
    created list in index.php

--- index.php
+++ index.php
@@ -11,8 +11,13 @@ require("includes/functions.inc.php");
   <h2>Select an option</h2>
   <p>
    <ul>
-    <li><a href="autopop.php">Manage resources/qb/qb-core/shared/vehicles.lua populator</a></li>
-    <li><a href="garpop.php">Manage resources/qb/qb-garages/config.lua populator</a></li>
+    <li>Manage resources\qb-core\shared\vehicles.lua
+     <li><a href="qb-core-shared-vehicles-populator">qb-core/shared/vehicles.lua populator</a></li>
+     <li><a href="qb-core-shared-vehicles-categories-list">List Vehicle Categories</a></li>
+    </li>
+    <li>Manage resources\qb-garages\config.lua
+     <li><a href="qb-garages-config-populator.php">qb-garages/config.lua populator</a></li>
+    </li>
    </ul>
   </p>
  </body>
