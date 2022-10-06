177067dff0a5bf5d30bf7f1ab5132cbbdf7fa68d
r0adra93 <github.com@legendsclan.org>
2022-10-06 14:42:03 -0400

    Update qb-core-shared-items-populator.php
    added the expire flag for some items have expiration periods

--- qb-core-shared-items-populator.php
+++ qb-core-shared-items-populator.php
@@ -11,14 +11,22 @@
  $timestamp = date('YmdHis', $timestamp);
  $output = "-- Date and Time Edited: " . date("Y-m-d H:i:s") . "\rQBShared = QBShared or {}\rQBShared.Items = {\r";
  foreach($sql1 as $k => $v){
-   if($v['enabled'] === 'true'){
-    $output .= " ['{$v['name']}'] = {['name'] = '{$v['name']}', ['label'] = '{$v['label']}', ['weight'] = {$v['weight']}, ['type'] = '{$v['type']}', ['ammotype'] = '{$v['ammotype']}', ['image'] = '{$v['image']}', ['unique'] = {$v['itemunique']}, ['useable'] = {$v['useable']}, ['description'] = \"{$v['description']}\", ['enabled'] = {$v['enabled']}},\r";
+  if($v['enabled'] === 'true'){
+   $output .= " ['{$v['name']}'] = {['name'] = '{$v['name']}', ['label'] = \"{$v['label']}\", ['weight'] = {$v['weight']}, ['type'] = '{$v['type']}', ['ammotype'] = '{$v['ammotype']}', ['image'] = '{$v['image']}', ['unique'] = {$v['itemunique']}, ['useable'] = {$v['useable']}, ['description'] = \"{$v['description']}\", ['enabled'] = {$v['enabled']}";
+   if(isset($v['expire'])){
+    $output .= ", ['expire'] = {$v['expire']}";
    }
+   $output .= "},\r\n";
+  }
  }
  foreach($sql2 as $k => $v){
-   if($v['enabled'] === 'true'){
-     $output .= " ['{$v['name']}'] = {['name'] = '{$v['name']}', ['label'] = '{$v['label']}', ['weight'] = {$v['weight']}, ['image'] = '{$v['image']}', ['unique'] = {$v['itemunique']}, ['useable'] = {$v['useable']}, ['shouldClose'] = {$v['shouldClose']}, ['combinable'] = {$v['combinable']}, ['description'] = \"{$v['description']}\", ['enabled'] = {$v['enabled']}},\r";
+  if($v['enabled'] === 'true'){
+   $output .= " ['{$v['name']}'] = {['name'] = '{$v['name']}', ['label'] = \"{$v['label']}\", ['weight'] = {$v['weight']}, ['image'] = '{$v['image']}', ['unique'] = {$v['itemunique']}, ['useable'] = {$v['useable']}, ['shouldClose'] = {$v['shouldClose']}, ['combinable'] = {$v['combinable']}, ['description'] = \"{$v['description']}\", ['enabled'] = {$v['enabled']}";
+   if(isset($v['expire'])){
+    $output .= ", ['expire'] = {$v['expire']}";
    }
+   $output .= "},\r\n";
+  }
  }
  $output .= "}";
  if(fwrite($fh,$output)){
