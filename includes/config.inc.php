5fdea30b0136cf55ba5db3c99b04bef91f674422
r0adra93 <github.com@legendsclan.org>
2022-10-06 15:41:11 -0400

    secured config
    Secured Configuration

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -3,14 +3,10 @@
  session_start();
  $sid = SID;
  $dbinfo = array();
- // Database Configuration Begins
- $dbinfo['username'] = "root"; // MySQL user name
- $dbinfo['password'] = ""; // MySQL Password
- $dbinfo['host'] = "localhost"; // MySQL Server Address
- $dbinfo['dbname'] = "phpqbadmin"; // Database Name
  $lang = "English"; // Set default language -- Currently only English is Supported
- define('RESOURCES', 'C:/Program Files/FXServer/txData/QBCoreFramework/resources'); // Set Path to Resources Folder
+ define('RESOURCES', 'C:/Program Files/fxserver-test/txData/QBCoreFramework/resources'); // Set Path to Resources Folder
  $messages = array();
+ require_once("{$_SERVER['DOCUMENT_ROOT']}/../secureconfigfiles/phpqbadmin-dbinfo.inc.php"); // Load Language File
  require_once('includes/resources.inc.php');
  require_once('includes/functions.inc.php');
 ?>
