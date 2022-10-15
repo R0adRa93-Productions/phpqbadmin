980317afacb03d0212994d74efe74e60b4f7269d
r0adra93 <github.com@legendsclan.org>
2022-10-15 10:58:08 -0400

    Update qb-garages-config-populator.php
    updated garage

--- qb-garages-config-populator.php
+++ qb-garages-config-populator.php
@@ -7,12 +7,12 @@
  $garloc = array();
  $query = "SELECT * FROM `qb-garages-config`";
  $garconf = sqlrun($query);
- $query = "SELECT * FROM `qb-garages-locations`";
+ $query = "SELECT * FROM `qb-garages-config-locations`";
  $garloc = sqlrun($query);
  $output = "-- Date and Time Edited: " . date("Y-m-d H:i:s") . "\r\nConfig = Config or {}\r\nConfig.AutoRespawn = {$garconf[0]['autorespawn']}\r\nConfig.SharedGarages = {$garconf[0]['sharedgarages']}\r\nConfig.VisuallyDamageCars = {$garconf[0]['visuallydamagecars']}\r\nConfig.Garages = {\r\n";
  foreach($garloc as $k => $v){
   if($v['enabled'] === 'true'){
-   $output .= " [\"{$v['name']}\"] = {[\"label\"] = \"{$v['label']}\",[\"blipName\"] = \"{$v['blipname']}\",[\"showBlip\"] = {$v['showblip']},[\"blipNumber\"] = {$v['blipnumber']},[\"takeVehicle\"] = {$v['takevehicle']},[\"spawnPoint\"] = {$v['spawnpoint']},[\"type\"] = \"{$v['type']}\",[\"vehicle\"] = \"{$v['vehicle']}\"";
+   $output .= " [\"{$v['name']}\"] = {[\"label\"] = \"{$v['label']}\",[\"blipName\"] = \"{$v['blipname']}\",[\"showBlip\"] = {$v['showblip']},[\"blipNumber\"] = {$v['blipnumber']},[\"blipColor\"] = {$v['blipcolor']},[\"takeVehicle\"] = {$v['takevehicle']},[\"spawnPoint\"] = {$v['spawnpoint']},[\"type\"] = \"{$v['type']}\",[\"vehicle\"] = \"{$v['vehicle']}\"";
    if($v['putvehicle']){
     $output .= ",[\"putVehicle\"] = {$v['putvehicle']}";
    }
